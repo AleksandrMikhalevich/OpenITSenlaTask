bdc37fe2c8fb94e64e0a3be3dcb5625b35b3f065
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-09-17 12:16:44 +0300

    Some files commited

---
+++ src/test/java/com/example/mikhalevich/MikhalevichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mikhalevich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhalevichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
